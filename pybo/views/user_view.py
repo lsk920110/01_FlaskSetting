952cdd4628e1e2ecff3380f2f4015f5a1c17e76b
lee seung kyu <lsk920110@gmail.com>
2022-04-14 08:24:13 +0900

    pycharm 에서 커밋test

--- pybo/views/user_view.py
+++ pybo/views/user_view.py
@@ -8,12 +8,13 @@ def login():
     return render_template('user/login.html')
 
 
-@bp.route('/join', methods=['GET','POST'])
+@bp.route('/join', methods=('GET','POST'))
 def join():
     if request.method == 'GET':
         return render_template('user/join.html')
     elif request.method == 'POST':
         print('post')
+        #print(request.form('id'))
         request.form('user_id')
         return 'post'
 
